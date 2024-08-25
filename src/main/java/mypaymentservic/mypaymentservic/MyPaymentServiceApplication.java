e3e46cd5cbc04e8aab6558dcd677fd71ada64460
gylee <leekhy02@naver.com>
2024-08-25 20:15:03 +0900

    Initial commit

---
+++ src/main/java/mypaymentservic/mypaymentservic/MyPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package mypaymentservic.mypaymentservic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mypaymentservic/mypaymentservic/MyPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mypaymentservic.mypaymentservic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
