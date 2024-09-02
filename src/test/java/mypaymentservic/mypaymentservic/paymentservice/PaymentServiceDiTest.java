cd59361f69b2cf42f78cb0e4fa164ebcbc42ed64
gylee <leekhy02@naver.com>
2024-09-02 23:12:22 +0900

    feat : paymentService Stub Autowired Test

--- src/test/java/mypaymentservic/mypaymentservic/paymentservice/PaymentServiceDiTest.java
+++ src/test/java/mypaymentservic/mypaymentservic/paymentservice/PaymentServiceDiTest.java
@@ -3,7 +3,6 @@ package mypaymentservic.mypaymentservic.paymentservice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -20,6 +19,8 @@ class PaymentServiceDiTest {
 
     @Autowired
     PaymentService paymentService;
+    @Autowired
+    ExRateProviderStub exRateProviderStub;
 
     @Test
     @DisplayName("prepare 메서드가 요구사항의 3가지 조건을 잘 충족햇는지 검증")
@@ -28,10 +29,12 @@ class PaymentServiceDiTest {
 
         assertThat(payment.getExRate()).isEqualByComparingTo(valueOf(1_000));
         assertThat(payment.getConvertedAmount()).isEqualByComparingTo(valueOf(10_000));
-        //환율 정보를 가져온다
-        //원화 환삼금액 계산
-        //우너화환삼금액의 유효시간 계산
-//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", TEN);
+
+        assertThat(payment2.getExRate()).isEqualByComparingTo(valueOf(500));
+        assertThat(payment2.getConvertedAmount()).isEqualByComparingTo(valueOf(5_000));
 
     }
 
